ec5f06027d3f05881332e1013d389d698594f9ed
Tino3141 <tino122001@googlemail.com>
2022-10-24 17:57:29 +0800

    Enhancing data Class Read From JSON Convert to JSON

--- src/app/iotdevice/lib/data_class.dart
+++ src/app/iotdevice/lib/data_class.dart
@@ -1,10 +1,11 @@
 class DataClass {
-  final double x;
-  final double y;
-  final double z;
-  final double temperature;
-  final double photoresistor;
-  final int timeStamp;
+  late double x;
+  late double y;
+  late double z;
+  late double temperature;
+  late double photoresistor;
+  late int timeStamp;
+  // location data is still missing
 
   DataClass(this.x, this.y, this.z,this.temperature, this.photoresistor, this.timeStamp);
   Map toJson() => {
@@ -14,5 +15,14 @@ class DataClass {
         'temp': temperature,
         'light': photoresistor,
         'time': timeStamp
-      };
+  };
+  
+  DataClass.fromJson(Map<String, dynamic> json)
+      : x = json['x'],
+        y = json['y'],
+        z = json['z'],
+        temperature = json['temp'],
+        photoresistor = json['light'],
+        timeStamp = json['time'];
+
 }
