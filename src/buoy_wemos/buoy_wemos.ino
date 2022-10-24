d0d954c3e1503b131327724ba42d598bd0df5948
leon-thomm <l.thomm@mailbox.org>
2022-10-24 19:30:56 +0800

    new main program
    successfully collects data from all sensors (including both I2C devices) and send to phone over HTTP

--- src/buoy_wemos/buoy_wemos.ino
+++ src/buoy_wemos/buoy_wemos.ino
@@ -1,22 +1,39 @@
+#include <ArduinoJson.h>
+
 #include "i2c/i2c.h"
 #include "adc/adc.h"
 #include "photores/photores.h"
 #include "temperature/temperature.h"
-// #include "mpu/mpu.h"
+#include "mpu/mpu.h"
+#include "wifi/wifi.h"
+
+#define MPU_ADDR 0x68
+#define ADC_ADDR 0x48   // hardwired
+#define FETCH_BASE_INT_MS 5000
 
 int adc_address;
+int last_fetch;
 
+void wait() {
+    int t = millis();
+    if(t - last_fetch < FETCH_BASE_INT_MS) {
+        delay(FETCH_BASE_INT_MS - (t - last_fetch));
+    }
+    last_fetch = millis();
+}
 
 void setup()
 {
-    Wire.begin();
     Serial.begin(9600);
+    while (!Serial) continue;
+    delay(2000);
 
-    // scan
+    // I2C
+
+    Wire.begin();
+    // scan: make sure we find both I2C devices
     Serial.println("scanning for I2C devices...");
     int* dev = i2c::scan(false);
-    Serial.println("done");
-
     Serial.println("found the following devices:");
     for(int i=0; i<dev[0]; i++) {
         Serial.print("device ");
@@ -26,47 +43,74 @@ void setup()
         Serial.println();
     }
 
-    // TODO: how to ensure the ADC's address is the first when using multiple I2C devices?
-    adc_address = dev[1];
+    while(dev[0] != 2){}
 
     // initialize sensor interfaces
-    adc::init(adc_address);
+    adc::init(ADC_ADDR);
     photores::init(0);
-    // temperature::init(1);
-    // mpu::init();
+    temperature::init(1);
+    mpu::init(MPU_ADDR);
+
+    // wifi
+    wifi::init();
 }
 
 void loop()
 {
-    // read ADC channel 0 (photo resistor)
-    Serial.print("photores:\t\t");
-    Serial.println(photores::read());
+
+    wait();
+
+    // read adc
+    float brightness = photores::read();
+    float heat = temperature::read();
+
+    // read mpu
+    xyzFloat acc = mpu::getG();
+    xyzFloat gyr = mpu::getGyr();
+    float res_g = mpu::getResG();
+
+    // generate json doc
+    StaticJsonDocument<200> doc;
+    doc["time"] = millis();
+    doc["temp"] = heat;
+    doc["light"] = brightness;
+    JsonArray acc_doc = doc.createNestedArray("acc");
+    acc_doc.add(acc.x);
+    acc_doc.add(acc.y);
+    acc_doc.add(acc.z);
+    acc_doc.add(res_g);
+    JsonArray gyr_doc = doc.createNestedArray("gyro");
+    gyr_doc.add(gyr.x);
+    gyr_doc.add(gyr.y);
+    gyr_doc.add(gyr.z);
+    serializeJson(doc, Serial);
+    Serial.println();
+
+    wifi::sendJSON(doc);
+
+    // print
+
+    // Serial.print("photores:\t\t");
+    // Serial.println(brightness);
     // Serial.print("temperature:\t\t");
-    // Serial.println(temperature::read());
+    // Serial.print(heat);
+    // Serial.println();
+
+    // Serial.print("acc [g] x,y,z,g:\t\t");
+    // Serial.print(gValue.x);
+    // Serial.print("\t\t");
+    // Serial.print(gValue.y);
+    // Serial.print("\t\t");
+    // Serial.print(gValue.z);
+    // Serial.print("\t\t");
+    // Serial.println(resultantG);
+
+    // Serial.print("gyro [deg/s] x,y,z:\t\t");
+    // Serial.print(gyr.x);
+    // Serial.print("\t\t");
+    // Serial.print(gyr.y);
+    // Serial.print("\t\t");
+    // Serial.println(gyr.z);
+
     delay(1000);
-
-    /*  mpu
-
-    xyzFloat gValue = mpu.getGValues();
-    xyzFloat gyr = mpu.getGyrValues();
-    float temp = mpu.getTemperature();
-    float resultantG = mpu.getResultantG(gValue);
-
-    Serial.print("acc [g] x,y,z,g:\t\t");
-    Serial.print(gValue.x);
-    Serial.print("\t\t");
-    Serial.print(gValue.y);
-    Serial.print("\t\t");
-    Serial.print(gValue.z);
-    Serial.print("\t\t");
-    Serial.println(resultantG);
-
-    Serial.print("gyro [deg/s] x,y,z:\t\t");
-    Serial.print(gyr.x);
-    Serial.print("\t\t");
-    Serial.print(gyr.y);
-    Serial.print("\t\t");
-    Serial.println(gyr.z);
-
-    */
 }
